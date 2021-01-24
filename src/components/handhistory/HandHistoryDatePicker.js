c22d6720e39ef821596e0c833706ec59a14bd9e2
Chris060986 <christoph.birk@gmail.com>
2021-01-24 17:29:35 +0100

    add 2 date pickers for range

--- src/components/handhistory/HandHistoryDatePicker.js
+++ src/components/handhistory/HandHistoryDatePicker.js
@@ -2,27 +2,69 @@ import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 
 import "react-datepicker/dist/react-datepicker.css";
+import './HandHistoryDatePicker.css'
 
 const MyDatePicker = () => {
     const [startDate, setStartDate] = useState(new Date().setDate(new Date().getDate()-1));
     const [endDate, setEndDate] = useState(new Date());
+    const CustomButtonInput = ({ value, onClick }) => (
+        <button className="button-input" onClick={onClick}>
+          {value}
+        </button>
+      );
     return (
         <>
+        <div className='datepicker-sub-div'>
         <DatePicker
-          selected={startDate}
-          onChange={date => setStartDate(date)}
-          selectsStart
-          startDate={startDate}
-          endDate={endDate}
+            selected={startDate}
+            onChange={date => setStartDate(date)}
+            selectsStart
+            startDate={startDate}
+            endDate={endDate}
+            customInput={<CustomButtonInput />}
+            timeInputLabel="Time:"
+            dateFormat="dd-MM-yyyy hh:mm aa"
+            showTimeInput
+            popperClassName="some-custom-class"
+            popperPlacement="top-end"
+            popperModifiers={{
+                offset: {
+                  enabled: true,
+                  offset: "-50px, 5px"
+                },
+                preventOverflow: {
+                  enabled: true,
+                  escapeWithReference: false,
+                  boundariesElement: "viewport"
+                }
+              }}
         />
+        </div>
+        <div className='datepicker-sub-div'>
         <DatePicker
-          selected={endDate}
-          onChange={date => setEndDate(date)}
-          selectsEnd
-          startDate={startDate}
-          endDate={endDate}
-          minDate={startDate}
+            customInput={<CustomButtonInput />}
+            selected={endDate}
+            onChange={date => setEndDate(date)}
+            selectsEnd
+            startDate={startDate}
+            endDate={endDate}
+            minDate={startDate}
+            timeInputLabel="Time:"
+            dateFormat="dd-MM-yyyy hh:mm aa"
+            showTimeInput
+            popperModifiers={{
+                offset: {
+                  enabled: true,
+                  offset: "-100px, 5px"
+                },
+                preventOverflow: {
+                  enabled: true,
+                  escapeWithReference: false,
+                  boundariesElement: "viewport"
+                }
+              }}
         />
+        </div>
       </>
     );
   };
