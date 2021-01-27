b66ed57e59741be8472e34b2a3a89d292f18f3ef
Chris060986 <christoph.birk@gmail.com>
2021-01-27 17:17:53 +0100

    Rework header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
@@ -6,8 +5,7 @@ import Profile from "./pages/Profile";
 import Handhistory from './pages/Handhistory';
 import Villains from './pages/Villains';
 
-
-function App() {
+function Old() {
   return (
     <>
       <Router>
@@ -21,6 +19,21 @@ function App() {
           <Route path='/villains' component={Villains}/>
         </Switch>
       </div>
+      </Router>
+    </>
+  );
+}
+
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Header></Header>
+          <Switch>
+            <Route path='/' exact component={Profile}/>
+            <Route path='/profile' component={Profile}/>
+          </Switch>
       </Router>
     </>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,34 @@
-import './Header.css'
-import * as DiIcons from 'react-icons/di';
 
-const Header = () => (
-    <div className='App-header'>
-        <DiIcons.DiReact className='Header-icon'></DiIcons.DiReact>
-        <span className='Header-title'>
-            The Poker Bro
-        </span>
-    </div>
-)
+import * as DiIcons from 'react-icons/di';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar'
+import TypoGraphy from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles';
+
+const appBarStyle = makeStyles((theme) => ({
+    header: {
+        backgroundColor: theme.palette.secondary.dark,
+    },
+    icon: {
+        height: "72px",
+        width: "72px",
+        color: theme.palette.primary.main,
+    }
+  }));
+
+function Header() {
+    
+    const classes = appBarStyle()
+    return (
+      <>
+        <AppBar className={classes.header} position="static">
+          <Toolbar>
+            <DiIcons.DiReact className={classes.icon}></DiIcons.DiReact>
+            <TypoGraphy variant="h4" component="h1" color="primary" >The Poker Bro</TypoGraphy>
+          </Toolbar>
+        </AppBar>
+      </>
+    )
+}
 
 export default Header
