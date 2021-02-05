85801bd960469f53b482af58ab85e611d0fd249d
Chris060986 <christoph.birk@gmail.com>
2021-02-05 18:17:21 +0100

    Make table row own component with collabsible box

--- src/components/handhistory/HandHistoryTable.js
+++ src/components/handhistory/HandHistoryTable.js
@@ -1,54 +1,11 @@
 import React from 'react'
 import './HandHistoryTable.css'
-import Cards from './Cards'
-import * as AiIcons from 'react-icons/ai';
-import { IconContext } from 'react-icons';
-import IconButton from '@material-ui/core/IconButton';
+import HandHistoryTableRow from './HandHistoryTableRow'
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@material-ui/core';
 
 class HandHistoryTable extends React.Component {
 
-  earningsInBB(earnings, bb){
-    return (earnings/bb).toFixed(1);
-  }
-
-  startingHand(doc){
-    const hero = doc.hero
-    let starthand = []
-    Object.values(doc.players).filter(value => value.name===hero).map((player) => (
-      Object.values(player.hand).map(card => (
-        starthand.push(card)
-      ))
-    ))
-    return starthand
-  }
-
-  position(doc){
-    return "UTG"
-  }
-
-  flopcards(doc){
-    if(doc.flop){
-      return Object.values(doc.flop.cards)
-    } 
-    return []
-  }
-
-  turncard(doc){
-    if(doc.turn){
-      return [doc.turn.cards[3]]
-    }
-    return []
-  }
-
-  rivercard(doc){
-    if(doc.river){
-      return [doc.river.cards[4]]
-    }
-    return []
-  }
-
-  render(){
+ render(){
       return (
       <>
         <TableContainer  >
@@ -70,35 +27,9 @@ class HandHistoryTable extends React.Component {
               </TableRow>
             </TableHead>
             <TableBody>
-            {Object.values(this.props.items).map((value) => (
-            <TableRow key={value.id}>
-              <TableCell>
-              <IconButton aria-label="Replay" color="primary">
-                <AiIcons.AiOutlineDownCircle />
-              </IconButton>
-              </TableCell>
-              <TableCell>{value.id}</TableCell>
-              <TableCell>
-                {Intl.DateTimeFormat('de', 
-                { 
-                  month: 'numeric',
-                  day: 'numeric',
-                  year: 'numeric',
-                  hour: 'numeric',
-                  minute: 'numeric' 
-                }).format(new Date(value.timestamp))}
-              </TableCell>
-              <TableCell>{value.sb}/{value.bb}</TableCell>
-              <TableCell>{this.position(value.doc)}</TableCell>
-              <TableCell><Cards cards={this.startingHand(value)}/></TableCell>
-              <TableCell align="center" style={{ paddingRight:"0px" }}><Cards cards={this.flopcards(value)}/></TableCell>
-              <TableCell align="center" style={{ paddingRight:"0px", paddingLeft:"0px" }}><Cards cards={this.turncard(value)}/></TableCell>
-              <TableCell align="center" style={{ paddingRight:"0px", paddingLeft:"0px" }}><Cards cards={this.rivercard(value)}/></TableCell>
-              <TableCell align="right">{value.earnings}</TableCell>
-              <TableCell align="right">{this.earningsInBB(value.earnings, value.bb)}</TableCell>
-              <TableCell>{value.winners}</TableCell>
-          </TableRow>
-          ))}
+              {Object.values(this.props.items).map((value) => (
+                <HandHistoryTableRow hand={value} />
+              ))}
             </TableBody>
           </Table>
         </TableContainer>
---
+++ src/components/handhistory/HandHistoryTableRow.js
@@ -0,0 +1,113 @@
+import React from 'react'
+import Cards from './Cards'
+import * as AiIcons from 'react-icons/ai';
+import IconButton from '@material-ui/core/IconButton';
+import { Box, Collapse, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@material-ui/core';
+
+class HandHistoryTableRow extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            collapsed: false,
+            handBackup: props.hand,
+            hand: props.hand
+          }
+    }
+
+    earningsInBB(earnings, bb){
+        return (earnings/bb).toFixed(1);
+    }
+
+    startingHand(doc){
+        const hero = doc.hero
+        let starthand = []
+        Object.values(doc.players).filter(value => value.name===hero).map((player) => (
+        Object.values(player.hand).map(card => (
+            starthand.push(card)
+        ))
+        ))
+        return starthand
+    }
+
+    position(doc){
+        return "UTG"
+    }
+
+    flopcards(doc){
+        if(doc.flop){
+        return Object.values(doc.flop.cards)
+        } 
+        return []
+    }
+
+    turncard(doc){
+        if(doc.turn){
+        return [doc.turn.cards[3]]
+        }
+        return []
+    }
+
+    rivercard(doc){
+        if(doc.river){
+        return [doc.river.cards[4]]
+        }
+        return []
+    }
+
+    toggleCollapsed(){
+        this.setState(collapsed => ({
+            collapsed: !this.state.collapsed
+          }));
+    }
+
+    render(){
+         
+
+        return (
+        <React.Fragment>
+            <>  
+                <TableRow key={this.state.hand.id}>
+                <TableCell>
+                <IconButton aria-label="Replay" color="primary" onClick={()=>this.toggleCollapsed()}>
+                    {this.state.open ? <AiIcons.AiOutlineUpCircle/> : <AiIcons.AiOutlineDownCircle />}
+                </IconButton>
+                </TableCell>
+                <TableCell>{this.state.hand.id}</TableCell>
+                <TableCell>
+                    {Intl.DateTimeFormat('de', 
+                    { 
+                    month: 'numeric',
+                    day: 'numeric',
+                    year: 'numeric',
+                    hour: 'numeric',
+                    minute: 'numeric' 
+                    }).format(new Date(this.state.hand.timestamp))}
+                </TableCell>
+                <TableCell>{this.state.hand.sb}/{this.state.hand.bb}</TableCell>
+                <TableCell>{this.position(this.state.hand.doc)}</TableCell>
+                <TableCell><Cards cards={this.startingHand(this.state.hand)}/></TableCell>
+                <TableCell align="center" style={{ paddingRight:"0px" }}><Cards cards={this.flopcards(this.state.hand)}/></TableCell>
+                <TableCell align="center" style={{ paddingRight:"0px", paddingLeft:"0px" }}><Cards cards={this.turncard(this.state.hand)}/></TableCell>
+                <TableCell align="center" style={{ paddingRight:"0px", paddingLeft:"0px" }}><Cards cards={this.rivercard(this.state.hand)}/></TableCell>
+                <TableCell align="right">{this.state.hand.earnings}</TableCell>
+                <TableCell align="right">{this.earningsInBB(this.state.hand.earnings, this.state.hand.bb)}</TableCell>
+                <TableCell>{this.state.hand.winners}</TableCell>
+            </TableRow>
+            <TableRow>
+                <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+                <Collapse in={this.state.collapsed} timeout="auto" unmountOnExit>
+                <Box margin={1}>
+                    <Typography variant="h6" gutterBottom component="div">
+                    History
+                    </Typography>
+                </Box>
+                </Collapse>
+                </TableCell>
+            </TableRow>
+            </> 
+        </React.Fragment>
+        );
+    }
+}
+export default HandHistoryTableRow;
