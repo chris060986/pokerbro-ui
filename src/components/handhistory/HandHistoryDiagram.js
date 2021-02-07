ac1674405647c366dc4379a09ed93ca656821274
Chris060986 <christoph.birk@gmail.com>
2021-02-07 15:21:21 +0100

    add paper box

--- src/components/handhistory/HandHistoryDiagram.js
+++ src/components/handhistory/HandHistoryDiagram.js
@@ -2,16 +2,19 @@ import React from 'react'
 import './HandHistoryDiagram.css'
 import { LineChart } from 'react-chartkick'
 import 'chart.js'
+import { Paper } from '@material-ui/core';
 
 class HandHistoryDiagram extends React.Component{
 
     render () {
       return (
+        <Paper elevation={3} style={{ maxWidth: "1050px", minWidth: "1050px" }}>
         <div className="graph-wrapper">
           <div className='graph-div'>
             <LineChart curve={false} messages={{empty: "No data"}} colors={["#1f25af"]} data={this.props.data} />
           </div>
         </div>
+        </Paper>
       ) 
     }
 
