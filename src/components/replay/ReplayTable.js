f8242cdee570239bf5f364acecc26e3560c6e239
Chris060986 <christoph.birk@gmail.com>
2021-02-24 18:56:52 +0100

    redo div styles in table view

--- src/components/replay/ReplayTable.js
+++ src/components/replay/ReplayTable.js
@@ -8,17 +8,69 @@ import { withStyles } from '@material-ui/core/styles';
 import PlayerAvatar from "./PlayerAvatar";
 
 const useStyles = theme => ({
-    tableBackground: {
-       backgroundColor: "grey",
-       width:"1100", 
-       height:"600px", 
-       backgroundRepeat: "no-repeat", 
-       backgroundSize: "1100px 600px", 
-       backgroundImage: 'url(' + pokerTableImage + ')',
-       display: "flex",
-       position: "relativ",
+   
+     handID: {
+         display: "block"
      },
-});
+     replayTableRoot: {
+        display: "block",
+        width: "100%",
+        minWidth: "1400px",
+        height: "700px"
+     },
+     tableWrapper: {
+         display: "block",
+         height: "700px"
+     },
+     pokerTable: {
+        display: "table",
+        float: "left",
+        width: "1100px", 
+        minWidth:"1100px", 
+        height:"600px",
+        minHeight: "600px", 
+        padding: "0px 5px 0px 0px",
+        marginRight: "24px"
+     },
+     tableBackground: {
+        backgroundColor: "grey",
+        width:"1100px", 
+        height:"600px", 
+        backgroundRepeat: "no-repeat", 
+        backgroundSize: "1100px 600px", 
+        backgroundImage: 'url(' + pokerTableImage + ')',
+      },
+      tableStats: {
+        display: "table",
+        width: "-webkit-fill-available",
+        minWidth:"300px", 
+        height: "492px", 
+        minHeight: "492px",
+        textAlign: "center",
+        marginLeft: "24px",
+        marginBottom: "8px"
+      },
+      tableStatsPaper: {
+          height: "100%"
+      },
+      tableActions: {
+        display: "table",
+        width: "-webkit-fill-available", 
+        minWidth:"300px",
+        height:"200px", 
+        minHeight: "200px",
+        alignItems: "center",
+        marginLeft: "24px"
+      },
+      tableActionButtonWrapper: {
+        width: "255px",
+        margin: "auto"
+      },
+      tablestateButtonWrapper: {
+        width: "300px",
+        margin: "auto"
+      }
+    });
 
 function shiftPlayers(hero, playersList){
     while(hero.valueOf()!=playersList[0].name.valueOf()) {
@@ -44,44 +96,47 @@ class ReplayTable extends React.Component {
     render(){
         const { classes } = this.props;
         return(
-            <Box margin={5, 1} padding={3} style={{ minWidth:"900px", height: "700px",  display: "flow-root"}} >
-                <Typography variant="h6" gutterBottom component="div">{this.state.hand.id}</Typography>
-                <Box style={{ width: "70%", minWidth:"600px", height:"600px", float: "left", padding: "0px 5px 0px 0px"}} >
-                    <Paper className={classes.tableBackground} elevation={3} >
-                    {Object.values(this.state.players).map((player, index) => (
-                                <PlayerAvatar id={player.seat} player={player} tablePos={index} style={{ position: "absolute" }}/>
-                            ))}
-                    </Paper>
-                </Box>
-                <Box style={{ width: "30%", height:"400px", minWidth:"300px", float: "left", textAlign: "center"}} >
-                    <Paper elevation={3}>
-                        <Typography variant="h6" gutterBottom component="div">Statistics</Typography>
-                        <List >
-                            {Object.values(this.state.players).map(player => (
-                                <>
-                                <ListItem>{player.name}</ListItem>
-                                <Divider />
-                                </>
-                            ))}
-                            
-                        </List>
-                    </Paper>
-                </Box>
-                <Box style={{ width: "30%", height:"200px", minWidth:"300px", float: "left", display: "flex", alignItems: "center"}} >
-                    <Paper elevation={3}>
-                        <div style={{margin: "auto"}}>
-                        <IconButton color="primary" ><MdIcons.MdFastRewind size="1.5em" ></MdIcons.MdFastRewind></IconButton>
-                        <IconButton color="secondary" ><RiIcons.RiRestartFill size="2em"></RiIcons.RiRestartFill></IconButton>
-                        <IconButton color="primary"><MdIcons.MdFastForward size="1.5em" ></MdIcons.MdFastForward></IconButton>
-                        <IconButton color="primary"><MdIcons.MdSkipNext size="1.5em"></MdIcons.MdSkipNext></IconButton>
-                        <div>
-                        <Button variant="contained" size="small" color="secondary" margin={2}>Flop</Button>
-                        <Button variant="contained" size="small" color="secondary" margin={2}>Turn</Button>
-                        <Button variant="contained" size="small" color="secondary" margin={2}>River</Button>
-                        <Button variant="contained" size="small" color="secondary" margin={2}>Showdown</Button>
-                        </div>
-                        </div>
-                    </Paper>
+            <Box className={classes.replayTableRoot} margin={5, 1} padding={3} >
+                <Typography className={classes.handID} variant="h6" gutterBottom component="div">{this.state.hand.id}</Typography>
+                <Box className={classes.tableWrapper}>
+                    <Box className={classes.pokerTable} >
+                        <Paper className={classes.tableBackground} elevation={3} >
+                        {Object.values(this.state.players).map((player, index) => (
+                                    <PlayerAvatar id={player.seat} player={player} tablePos={index} style={{ position: "relative" }}/>
+                                    
+                                ))}
+                        </Paper>
+                    </Box>
+                    <Box className={classes.tableStats} >
+                        <Paper className={classes.tableStatsPaper} elevation={3}>
+                            <Typography variant="h6" gutterBottom component="div">Statistics</Typography>
+                            <List >
+                                {Object.values(this.state.players).map(player => (
+                                    <>
+                                    <ListItem>{player.name}</ListItem>
+                                    <Divider />
+                                    </>
+                                ))}
+                                
+                            </List>
+                        </Paper>
+                    </Box>
+                    <Box className={classes.tableActions} >
+                        <Paper elevation={3}>
+                            <Box className={classes.tableActionButtonWrapper}>
+                                <IconButton color="primary" ><MdIcons.MdFastRewind size="1.5em" ></MdIcons.MdFastRewind></IconButton>
+                                <IconButton color="secondary" ><RiIcons.RiRestartFill size="2em"></RiIcons.RiRestartFill></IconButton>
+                                <IconButton color="primary"><MdIcons.MdFastForward size="1.5em" ></MdIcons.MdFastForward></IconButton>
+                                <IconButton color="primary"><MdIcons.MdSkipNext size="1.5em"></MdIcons.MdSkipNext></IconButton>
+                            </Box>
+                            <Box className={classes.tablestateButtonWrapper}>
+                                <Button variant="contained" size="small" color="secondary" margin={2}>Flop</Button>
+                                <Button variant="contained" size="small" color="secondary" margin={2}>Turn</Button>
+                                <Button variant="contained" size="small" color="secondary" margin={2}>River</Button>
+                                <Button variant="contained" size="small" color="secondary" margin={2}>Showdown</Button>
+                            </Box>
+                        </Paper>
+                    </Box>
                 </Box>
             </Box>
         )
